eef1213af4e06470ca37d54d7a8e87b05cd442a7
brian-bayarri <brianbayarri@gmail.com>
2020-08-18 15:05:11 -0300

    Swagger section

---
+++ MoviesServices/MoviesServices/MoviesServices/Swagger/README.md
@@ -0,0 +1,13 @@
+# How to use Swagger?
+
+## Starting 📋
+
+* With the API running, go to localhost/swagger/index.html 
+* Once you are there, go to Login section and introduce your credentials
+* The API will give you a Token Code. Copy it
+
+## Test ⚙️
+
+* For the next step, you have to click on the Authorize button and a new window will be open
+* Now, you have to write "Bearer <your token code>" and click Authorize button
+* If everything goes fine, you are now authorize to test all the behavior of the API
---
+++ MoviesServices/MoviesServices/Swagger/SwaggerConfig.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+
+namespace MoviesServices.Swagger
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddRegistration(this IServiceCollection services)
+        {
+            var basePath = System.AppDomain.CurrentDomain.BaseDirectory;
+            var xmlPath = Path.Combine(basePath, "MoviesServices.xml");
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("MoviesServices", new OpenApiInfo { Title = "MoviesServices", Version = "v1" });
+                c.IncludeXmlComments(xmlPath);
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder AddRegistration (this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/MoviesServices/swagger.json", "MoviesServices"));
+
+            return app;
+        }
+    }
+}
