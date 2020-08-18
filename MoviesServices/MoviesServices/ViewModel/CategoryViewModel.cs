c532761da83db7f210165e7f8f55de38ea668927
brian-bayarri <brianbayarri@gmail.com>
2020-08-18 15:03:59 -0300

    News ViewModels for entities

---
+++ MoviesServices/MoviesServices/ViewModel/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesServices.ViewModel
+{
+    public class CategoryViewModel
+    {
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+    }
+}
---
+++ MoviesServices/MoviesServices/ViewModel/MovieViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesServices.ViewModel
+{
+    public class MovieViewModel
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string categoryName { get; set; }
+    }
+}
