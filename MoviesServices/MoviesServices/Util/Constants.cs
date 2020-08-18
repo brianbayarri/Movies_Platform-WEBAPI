125b07f66b0c291123c1783ab807883beda7127d
brian-bayarri <brianbayarri@gmail.com>
2020-08-18 15:04:16 -0300

    New utils classes

---
+++ MoviesServices/MoviesServices/Util/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MoviesServices.Util
+{
+    public static class Constants
+    {
+        public const int ADMIN_TYPE = 1;
+        public const int VIEWER_TYPE = 2;
+        public const int SUCCESS = 1;
+        public const int FAILURE = 0;
+        public const int CATEGORY_HAS_ASSOCIATED_MOVIES = -1;
+        public static int USER_ID;
+    }
+}
